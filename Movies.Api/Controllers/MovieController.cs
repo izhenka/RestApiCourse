cbd3548a0e50b03488f6b7d962e483a5e5a15ed5
eugeniahelgedagsrud <eugenia.helgedagsrud@montel.energy>
2025-03-01 19:02:41 +0100

    Added controller

---
+++ Movies.Api/Controllers/MovieController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class MovieController: ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MovieController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+    
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+        await _movieRepository.CreateAsync(movie);
+        return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
+    }
+    
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        var movie = await _movieRepository.GetByIdAsync(id);
+        if (movie is null)
+        {
+            return NotFound();
+        }
+        return Ok(movie.MapToResponse());
+    }
+    
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAll()
+    {
+        var movies = await _movieRepository.GetAllAsync();
+        return Ok(movies.MapToResponse());
+    }
+    
+
+    
+} 
